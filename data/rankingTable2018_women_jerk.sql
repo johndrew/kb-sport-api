005c3ec9a6a54381cacb1f7795862e43fee04606
Taylor Johndrew <johndrew.t@gmail.com>
2018-08-26 09:40:38 -0700

    Added jerk tables

---
+++ data/rankingTable2018_women_jerk.sql
@@ -0,0 +1,185 @@
+-- MSIC: PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(72,  1, 'Jerk', '10min', 'Strawweight',        '24', 'PRO', 'women'),
+(77,  1, 'Jerk', '10min', 'Flyweight',          '24', 'PRO', 'women'),
+(83,  1, 'Jerk', '10min', 'Bantamweight',       '24', 'PRO', 'women'),
+(90,  1, 'Jerk', '10min', 'Featherweight',      '24', 'PRO', 'women'),
+(96,  1, 'Jerk', '10min', 'Lightweight',        '24', 'PRO', 'women'),
+(102, 1, 'Jerk', '10min', 'Super Lightweight',  '24', 'PRO', 'women'),
+(109, 1, 'Jerk', '10min', 'Welterweight',       '24', 'PRO', 'women'),
+(116, 1, 'Jerk', '10min', 'Super Welterweight', '24', 'PRO', 'women');
+
+-- MS: PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(51,  2, 'Jerk', '10min', 'Strawweight',        '24', 'PRO', 'women'),
+(58,  2, 'Jerk', '10min', 'Flyweight',          '24', 'PRO', 'women'),
+(65,  2, 'Jerk', '10min', 'Bantamweight',       '24', 'PRO', 'women'),
+(72,  2, 'Jerk', '10min', 'Featherweight',      '24', 'PRO', 'women'),
+(79,  2, 'Jerk', '10min', 'Lightweight',        '24', 'PRO', 'women'),
+(86,  2, 'Jerk', '10min', 'Super Lightweight',  '24', 'PRO', 'women'),
+(93,  2, 'Jerk', '10min', 'Welterweight',       '24', 'PRO', 'women'),
+(99,  2, 'Jerk', '10min', 'Super Welterweight', '24', 'PRO', 'women');
+
+-- CMS: PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(41, 3, 'Jerk', '10min', 'Strawweight',        '24', 'PRO', 'women'),
+(46, 3, 'Jerk', '10min', 'Flyweight',          '24', 'PRO', 'women'),
+(52, 3, 'Jerk', '10min', 'Bantamweight',       '24', 'PRO', 'women'),
+(58, 3, 'Jerk', '10min', 'Featherweight',      '24', 'PRO', 'women'),
+(64, 3, 'Jerk', '10min', 'Lightweight',        '24', 'PRO', 'women'),
+(69, 3, 'Jerk', '10min', 'Super Lightweight',  '24', 'PRO', 'women'),
+(74, 3, 'Jerk', '10min', 'Welterweight',       '24', 'PRO', 'women'),
+(79, 3, 'Jerk', '10min', 'Middleweight',       '24', 'PRO', 'women');
+-- CMS: Semi PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(72,  3, 'Jerk', '10min', 'Strawweight',        '20', 'Semi PRO', 'women'),
+(77,  3, 'Jerk', '10min', 'Flyweight',          '20', 'Semi PRO', 'women'),
+(83,  3, 'Jerk', '10min', 'Bantamweight',       '20', 'Semi PRO', 'women'),
+(90,  3, 'Jerk', '10min', 'Featherweight',      '20', 'Semi PRO', 'women'),
+(96,  3, 'Jerk', '10min', 'Lightweight',        '20', 'Semi PRO', 'women'),
+(102, 3, 'Jerk', '10min', 'Super Lightweight',  '20', 'Semi PRO', 'women'),
+(106, 3, 'Jerk', '10min', 'Welterweight',       '20', 'Semi PRO', 'women'),
+(116, 3, 'Jerk', '10min', 'Middleweight',       '20', 'Semi PRO', 'women');
+-- CMS: Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(88,  3, 'Jerk', '10min', 'Strawweight',        '16', 'Amateur', 'women'),
+(96,  3, 'Jerk', '10min', 'Flyweight',          '16', 'Amateur', 'women'),
+(104, 3, 'Jerk', '10min', 'Bantamweight',       '16', 'Amateur', 'women'),
+(112, 3, 'Jerk', '10min', 'Featherweight',      '16', 'Amateur', 'women'),
+(121, 3, 'Jerk', '10min', 'Lightweight',        '16', 'Amateur', 'women'),
+(131, 3, 'Jerk', '10min', 'Super Lightweight',  '16', 'Amateur', 'women'),
+(141, 3, 'Jerk', '10min', 'Welterweight',       '16', 'Amateur', 'women'),
+(150, 3, 'Jerk', '10min', 'Middleweight',       '16', 'Amateur', 'women');
+
+-- Rank I: PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(33, 4, 'Jerk', '10min', 'Strawweight',        '24', 'PRO', 'women'),
+(37, 4, 'Jerk', '10min', 'Flyweight',          '24', 'PRO', 'women'),
+(42, 4, 'Jerk', '10min', 'Bantamweight',       '24', 'PRO', 'women'),
+(46, 4, 'Jerk', '10min', 'Featherweight',      '24', 'PRO', 'women'),
+(51, 4, 'Jerk', '10min', 'Lightweight',        '24', 'PRO', 'women'),
+(55, 4, 'Jerk', '10min', 'Super Lightweight',  '24', 'PRO', 'women'),
+(60, 4, 'Jerk', '10min', 'Welterweight',       '24', 'PRO', 'women'),
+(63, 4, 'Jerk', '10min', 'Middleweight',       '24', 'PRO', 'women');
+-- Rank I: Semi PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(49,  4, 'Jerk', '10min', 'Strawweight',        '20', 'Semi PRO', 'women'),
+(55,  4, 'Jerk', '10min', 'Flyweight',          '20', 'Semi PRO', 'women'),
+(62,  4, 'Jerk', '10min', 'Bantamweight',       '20', 'Semi PRO', 'women'),
+(69,  4, 'Jerk', '10min', 'Featherweight',      '20', 'Semi PRO', 'women'),
+(76,  4, 'Jerk', '10min', 'Lightweight',        '20', 'Semi PRO', 'women'),
+(83,  4, 'Jerk', '10min', 'Super Lightweight',  '20', 'Semi PRO', 'women'),
+(90,  4, 'Jerk', '10min', 'Welterweight',       '20', 'Semi PRO', 'women'),
+(95,  4, 'Jerk', '10min', 'Middleweight',       '20', 'Semi PRO', 'women');
+-- Rank I: Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(69,  4, 'Jerk', '10min', 'Strawweight',        '16', 'Amateur', 'women'),
+(76,  4, 'Jerk', '10min', 'Flyweight',          '16', 'Amateur', 'women'),
+(83,  4, 'Jerk', '10min', 'Bantamweight',       '16', 'Amateur', 'women'),
+(90,  4, 'Jerk', '10min', 'Featherweight',      '16', 'Amateur', 'women'),
+(97,  4, 'Jerk', '10min', 'Lightweight',        '16', 'Amateur', 'women'),
+(105, 4, 'Jerk', '10min', 'Super Lightweight',  '16', 'Amateur', 'women'),
+(113, 4, 'Jerk', '10min', 'Welterweight',       '16', 'Amateur', 'women'),
+(121, 4, 'Jerk', '10min', 'Middleweight',       '16', 'Amateur', 'women');
+-- Rank I: Semi Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(83,  4, 'Jerk', '10min', 'Strawweight',        '12', 'Semi Amateur', 'women'),
+(91,  4, 'Jerk', '10min', 'Flyweight',          '12', 'Semi Amateur', 'women'),
+(99,  4, 'Jerk', '10min', 'Bantamweight',       '12', 'Semi Amateur', 'women'),
+(107, 4, 'Jerk', '10min', 'Featherweight',      '12', 'Semi Amateur', 'women'),
+(117, 4, 'Jerk', '10min', 'Lightweight',        '12', 'Semi Amateur', 'women'),
+(127, 4, 'Jerk', '10min', 'Super Lightweight',  '12', 'Semi Amateur', 'women'),
+(137, 4, 'Jerk', '10min', 'Welterweight',       '12', 'Semi Amateur', 'women'),
+(146, 4, 'Jerk', '10min', 'Middleweight',       '12', 'Semi Amateur', 'women');
+-- Rank I: Beginners Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(104, 4, 'Jerk', '10min', 'Strawweight',        '8', 'Beginners', 'women'),
+(112, 4, 'Jerk', '10min', 'Flyweight',          '8', 'Beginners', 'women'),
+(120, 4, 'Jerk', '10min', 'Bantamweight',       '8', 'Beginners', 'women'),
+(128, 4, 'Jerk', '10min', 'Featherweight',      '8', 'Beginners', 'women'),
+(138, 4, 'Jerk', '10min', 'Lightweight',        '8', 'Beginners', 'women'),
+(149, 4, 'Jerk', '10min', 'Super Lightweight',  '8', 'Beginners', 'women'),
+(160, 4, 'Jerk', '10min', 'Welterweight',       '8', 'Beginners', 'women'),
+(170, 4, 'Jerk', '10min', 'Middleweight',       '8', 'Beginners', 'women');
+
+-- Rank II: Semi PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(39, 5, 'Jerk', '10min', 'Strawweight',        '20', 'Semi PRO', 'women'),
+(44, 5, 'Jerk', '10min', 'Flyweight',          '20', 'Semi PRO', 'women'),
+(50, 5, 'Jerk', '10min', 'Bantamweight',       '20', 'Semi PRO', 'women'),
+(55, 5, 'Jerk', '10min', 'Featherweight',      '20', 'Semi PRO', 'women'),
+(61, 5, 'Jerk', '10min', 'Lightweight',        '20', 'Semi PRO', 'women'),
+(66, 5, 'Jerk', '10min', 'Super Lightweight',  '20', 'Semi PRO', 'women'),
+(71, 5, 'Jerk', '10min', 'Welterweight',       '20', 'Semi PRO', 'women'),
+(76, 5, 'Jerk', '10min', 'Middleweight',       '20', 'Semi PRO', 'women');
+-- Rank II: Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(55,  5, 'Jerk', '10min', 'Strawweight',        '16', 'Amateur', 'women'),
+(61,  5, 'Jerk', '10min', 'Flyweight',          '16', 'Amateur', 'women'),
+(67,  5, 'Jerk', '10min', 'Bantamweight',       '16', 'Amateur', 'women'),
+(73,  5, 'Jerk', '10min', 'Featherweight',      '16', 'Amateur', 'women'),
+(79,  5, 'Jerk', '10min', 'Lightweight',        '16', 'Amateur', 'women'),
+(85,  5, 'Jerk', '10min', 'Super Lightweight',  '16', 'Amateur', 'women'),
+(91,  5, 'Jerk', '10min', 'Welterweight',       '16', 'Amateur', 'women'),
+(97,  5, 'Jerk', '10min', 'Middleweight',       '16', 'Amateur', 'women');
+-- Rank II: Semi Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(67,  5, 'Jerk', '10min', 'Strawweight',        '12', 'Semi Amateur', 'women'),
+(73,  5, 'Jerk', '10min', 'Flyweight',          '12', 'Semi Amateur', 'women'),
+(79,  5, 'Jerk', '10min', 'Bantamweight',       '12', 'Semi Amateur', 'women'),
+(86,  5, 'Jerk', '10min', 'Featherweight',      '12', 'Semi Amateur', 'women'),
+(94,  5, 'Jerk', '10min', 'Lightweight',        '12', 'Semi Amateur', 'women'),
+(102, 5, 'Jerk', '10min', 'Super Lightweight',  '12', 'Semi Amateur', 'women'),
+(110, 5, 'Jerk', '10min', 'Welterweight',       '12', 'Semi Amateur', 'women'),
+(117, 5, 'Jerk', '10min', 'Middleweight',       '12', 'Semi Amateur', 'women');
+-- Rank II: Beginners Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(83,  5, 'Jerk', '10min', 'Strawweight',        '8', 'Beginners', 'women'),
+(89,  5, 'Jerk', '10min', 'Flyweight',          '8', 'Beginners', 'women'),
+(96,  5, 'Jerk', '10min', 'Bantamweight',       '8', 'Beginners', 'women'),
+(103, 5, 'Jerk', '10min', 'Featherweight',      '8', 'Beginners', 'women'),
+(110, 5, 'Jerk', '10min', 'Lightweight',        '8', 'Beginners', 'women'),
+(118, 5, 'Jerk', '10min', 'Super Lightweight',  '8', 'Beginners', 'women'),
+(126, 5, 'Jerk', '10min', 'Welterweight',       '8', 'Beginners', 'women'),
+(134, 5, 'Jerk', '10min', 'Middleweight',       '8', 'Beginners', 'women');
+
+-- Rank III: Semi PRO Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(31, 6, 'Jerk', '10min', 'Strawweight',        '20', 'Semi PRO', 'women'),
+(35, 6, 'Jerk', '10min', 'Flyweight',          '20', 'Semi PRO', 'women'),
+(40, 6, 'Jerk', '10min', 'Bantamweight',       '20', 'Semi PRO', 'women'),
+(44, 6, 'Jerk', '10min', 'Featherweight',      '20', 'Semi PRO', 'women'),
+(49, 6, 'Jerk', '10min', 'Lightweight',        '20', 'Semi PRO', 'women'),
+(53, 6, 'Jerk', '10min', 'Super Lightweight',  '20', 'Semi PRO', 'women'),
+(58, 6, 'Jerk', '10min', 'Welterweight',       '20', 'Semi PRO', 'women'),
+(61, 6, 'Jerk', '10min', 'Middleweight',       '20', 'Semi PRO', 'women');
+-- Rank III: Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(44, 6, 'Jerk', '10min', 'Strawweight',        '16', 'Amateur', 'women'),
+(49, 6, 'Jerk', '10min', 'Flyweight',          '16', 'Amateur', 'women'),
+(53, 6, 'Jerk', '10min', 'Bantamweight',       '16', 'Amateur', 'women'),
+(58, 6, 'Jerk', '10min', 'Featherweight',      '16', 'Amateur', 'women'),
+(62, 6, 'Jerk', '10min', 'Lightweight',        '16', 'Amateur', 'women'),
+(67, 6, 'Jerk', '10min', 'Super Lightweight',  '16', 'Amateur', 'women'),
+(72, 6, 'Jerk', '10min', 'Welterweight',       '16', 'Amateur', 'women'),
+(77, 6, 'Jerk', '10min', 'Middleweight',       '16', 'Amateur', 'women');
+-- Rank III: Semi Amateur Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(53,  6, 'Jerk', '10min', 'Strawweight',        '12', 'Semi Amateur', 'women'),
+(58,  6, 'Jerk', '10min', 'Flyweight',          '12', 'Semi Amateur', 'women'),
+(63,  6, 'Jerk', '10min', 'Bantamweight',       '12', 'Semi Amateur', 'women'),
+(69,  6, 'Jerk', '10min', 'Featherweight',      '12', 'Semi Amateur', 'women'),
+(75,  6, 'Jerk', '10min', 'Lightweight',        '12', 'Semi Amateur', 'women'),
+(81,  6, 'Jerk', '10min', 'Super Lightweight',  '12', 'Semi Amateur', 'women'),
+(87,  6, 'Jerk', '10min', 'Welterweight',       '12', 'Semi Amateur', 'women'),
+(93,  6, 'Jerk', '10min', 'Middleweight',       '12', 'Semi Amateur', 'women');
+-- Rank III: Beginners Division
+INSERT INTO womenRankingTable (score, ranking, eventType, duration, weightCategory, kettlebellWeight, division, gender) VALUES
+(67,  6, 'Jerk', '10min', 'Strawweight',        '8', 'Beginners', 'women'),
+(72,  6, 'Jerk', '10min', 'Flyweight',          '8', 'Beginners', 'women'),
+(77,  6, 'Jerk', '10min', 'Bantamweight',       '8', 'Beginners', 'women'),
+(82,  6, 'Jerk', '10min', 'Featherweight',      '8', 'Beginners', 'women'),
+(88,  6, 'Jerk', '10min', 'Lightweight',        '8', 'Beginners', 'women'),
+(95,  6, 'Jerk', '10min', 'Super Lightweight',  '8', 'Beginners', 'women'),
+(102, 6, 'Jerk', '10min', 'Welterweight',       '8', 'Beginners', 'women'),
+(109, 6, 'Jerk', '10min', 'Middleweight',       '8', 'Beginners', 'women');
