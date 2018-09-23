068d50405f65b6fbfd2e1de6380da0c9843402ba
Taylor Johndrew <johndrew.t@gmail.com>
2018-09-23 14:47:29 -0700

    Removed unneeded scripts for docker

--- resources/dockerScripts/sqlDump.sh
+++
@@ -1,8 +0,0 @@
-mysqldump \
-    --databases rankingTable2018 \
-    --master-data=2  \
-    --single-transaction \
-    --order-by-primary \
-    -r /tmp/tmpsql/backup.sql \
-    -uroot \
-    -ppassword
