df7865dfa95ad7552bb6017f1a19811fbac0cc75
Taylor Johndrew <johndrew.t@gmail.com>
2018-10-27 08:28:08 -0700

    Added webpack build for event db function

---
+++ server/eventDbInterfaceFunction/webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const path = require('path');
+const ZipPlugin = require('zip-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    handler: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `handler.js`,
+    library: 'handler',
+    libraryTarget: 'commonjs2'
+  },
+  plugins: [
+    new ZipPlugin(),
+  ],
+};
