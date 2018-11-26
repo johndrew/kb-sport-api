4839693a6354ce10fbcd39dfb0afeca413dde81e
Taylor Johndrew <johndrew.t@gmail.com>
2018-11-25 16:29:23 -0800

    feat(GetScore): Added lambda function

---
+++ server/getScoreFunction/index.js
@@ -0,0 +1,62 @@
+const VALID_FORMULA_TYPES = Object.freeze({
+    LONG_CYCLE: 'longCycle',
+    SNATCH: 'snatch',
+    JERK: 'jerk',
+    OALC: 'OALC',
+});
+
+function validateEvent(event) {
+    
+    if (!event) throw new Error('event is required');
+    if (!event.formulaType) throw new Error('formulaType is required');
+    if (Object.values(VALID_FORMULA_TYPES).indexOf(event.formulaType) < 0) throw new Error(`formulaType ${event.formulaType} is invalid`);
+    if (!event.kettlebellWeight) throw new Error('kettlebellWeight is required');
+    if (!event.weight) throw new Error('lifter weight is required');
+    if (!event.totalRepetitions) throw new Error('total repetitions is required');
+}
+
+exports.handler = async (event, context) => {
+
+    validateEvent(event);
+
+    let indexA;
+    let indexB;
+    let indexC;
+    let indexD;
+
+    switch (event.formulaType) {
+        case VALID_FORMULA_TYPES.LONG_CYCLE:
+            indexA = 3.1;
+            indexB = 160;
+            indexC = 1.003;
+            indexD = 7;
+            break;
+        case VALID_FORMULA_TYPES.SNATCH:
+            indexA = 2.3;
+            indexB = 80;
+            indexC = 1.0015;
+            indexD = 10;
+            break;
+        case VALID_FORMULA_TYPES.JERK:
+            indexA = 3.1
+            indexB = 160
+            indexC = 1.002
+            indexD = 20
+            break;
+        case VALID_FORMULA_TYPES.OALC:
+            indexA = 2.4
+            indexB = 80
+            indexC = 1.002
+            indexD = 7
+            break;
+        default:
+            console.warn(`formula type ${formulaType} is not recognized`);
+            break;
+    }
+
+    return (
+        Math.pow((event.kettlebellWeight / 6), indexA) *
+        ((event.weight + indexB) / event.weight) * 
+        (Math.pow(indexC, event.totalRepetitions) * event.totalRepetitions)
+    ) / indexD;
+}
---
+++ server/getScoreFunction/index.test.js
@@ -0,0 +1,267 @@
+const assert = require('assert');
+const { handler } = require('./index');
+
+const TEN_DIGIT_REGEX = /\d*\.(\d{1,10})/;
+
+/**
+ * Determines if two scores are equal.
+ * Equality is determined to ten decimal places.
+ */
+function assertScoresEqual(actual, expected) {
+
+    assert.strictEqual(
+        TEN_DIGIT_REGEX.exec(actual.toFixed(12))[1],
+        TEN_DIGIT_REGEX.exec(expected.toFixed(12))[1]
+    );
+}
+
+describe(__filename, () => {
+
+    const scoreEvent = {
+        formulaType: 'longCycle',
+        kettlebellWeight: 20,
+        weight: 98.9,
+        totalRepetitions: 28,
+    };
+    const context = {};
+
+    describe('Positive Tests', () => {
+
+        describe('when a long cycle score is requested', () => {
+
+            let event;
+            beforeEach(() => {
+                
+                event = Object.assign({}, scoreEvent, {
+                    formulaType: 'longCycle',
+                });
+            });
+
+            it('returns 475.714057454398 if kb=20,w=98.9,reps=28', async () => {
+
+                event = Object.assign(event, {
+                    kettlebellWeight: 20,
+                    weight: 98.9,
+                    totalRepetitions: 28,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 475.714057454398);
+            });
+    
+            it('returns 950.752130352771 if kb=16,w=90,reps=88', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 16,
+                    weight: 90,
+                    totalRepetitions: 88,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 950.752130352771);
+            });
+
+            it('returns 516.43444327462 if kb=12,w=75.3,reps=100', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 12,
+                    weight: 75.3,
+                    totalRepetitions: 100,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 516.43444327462);
+            });
+        });
+
+        describe('when a snatch score is requested', () => {
+
+            let event;
+            beforeEach(() => {
+                
+                event = Object.assign({}, scoreEvent, {
+                    formulaType: 'snatch',
+                });
+            });
+            
+            it('returns 604.832899651963 if kb=20,w=98.9,reps=164', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 20,
+                    weight: 98.9,
+                    totalRepetitions: 164,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 604.832899651963);
+            });
+
+            it('returns 242.150379487144 if kb=12,w=82.2,reps=188', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 12,
+                    weight: 82.2,
+                    totalRepetitions: 188,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 242.150379487144);
+            });
+        });
+
+        describe('when a jerk score is requested', () => {
+
+            let event;
+            beforeEach(() => {
+                
+                event = Object.assign({}, scoreEvent, {
+                    formulaType: 'jerk',
+                });
+            });
+            
+            it('returns 2274.87145895057 if kb=32,w=78,reps=72', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 32,
+                    weight: 78,
+                    totalRepetitions: 72,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 2274.87145895057);
+            });
+
+            it('returns 64.6602804872 if kb=8,w=49.4,reps=102', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 8,
+                    weight: 49.4,
+                    totalRepetitions: 102,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 64.6602804872);
+            });
+        });
+
+        describe('when a OALC request is made', () => {
+            
+            let event;
+            beforeEach(() => {
+                
+                event = Object.assign({}, scoreEvent, {
+                    formulaType: 'OALC',
+                });
+            });
+            
+            it('returns 221.521996678926 if kb=16,w=80.8,reps=65', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 16,
+                    weight: 80.8,
+                    totalRepetitions: 65,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 221.521996678926);
+            });
+
+            it('returns 34.5435719947451 if kb=8,w=85.5,reps=56', async () => {
+                
+                event = Object.assign(event, {
+                    kettlebellWeight: 8,
+                    weight: 85.5,
+                    totalRepetitions: 56,
+                });
+                
+                const actual = await handler(event, context);
+                assertScoresEqual(actual, 34.5435719947451);
+            });
+        });
+    });
+    
+    describe('Negative Tests', () => {
+
+        it('should error if event is missing', async () => {
+            
+            try {
+                await handler(null, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if event is missing');
+        });
+
+        it('should error if formula type is missing', async () => {
+
+            const event = Object.assign({}, scoreEvent, { formulaType: undefined });
+            
+            try {
+                await handler(event, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if formula type is missing');
+        });
+
+        it('should error if invalid formula type is provided', async () => {
+
+            const event = Object.assign({}, scoreEvent, { formulaType: 'foo' });
+            
+            try {
+                await handler(event, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if formula type is invalid');
+        });
+
+        it('should error if kettlebell weight is missing', async () => {
+
+            const event = Object.assign({}, scoreEvent, { kettlebellWeight: undefined });
+            
+            try {
+                await handler(event, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if kettlebell weight is missing');
+        });
+
+        it('should error if lifter weight is missing', async () => {
+
+            const event = Object.assign({}, scoreEvent, { weight: undefined });
+            
+            try {
+                await handler(event, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if lifter weight is missing');
+        });
+
+        it('should error if total repetitions is missing', async () => {
+
+            const event = Object.assign({}, scoreEvent, { totalRepetitions: undefined });
+            
+            try {
+                await handler(event, context);
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            
+            throw new Error('should not resolve if total repetitions is missing');
+        });
+    });
+});
